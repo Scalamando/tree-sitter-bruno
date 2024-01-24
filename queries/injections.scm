8a31f17b0fba12a388935aa1dc5e5274d6aa3b63
Raimund Canzler <git@rai-canzler.de>
2024-01-24 21:34:24 +0100

    feat: body and script injections

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,5 +1,47 @@
+((body
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "json"))
+
 ((bodyjson
 	(keyword)
 	(textblock
 		(rawtext) @injection.content))
  	(#set! injection.language "json"))
+
+((bodyxml
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "xml"))
+
+((bodygraphql
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "graphql"))
+
+((bodygraphqlvars
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "json"))
+
+((scriptres
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "javascript"))
+
+((scriptreq
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "javascript"))
+
+((tests
+	(keyword)
+	(textblock
+		(rawtext) @injection.content))
+ 	(#set! injection.language "javascript"))
